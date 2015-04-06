163946798a0b04f4cd0ce3818508b2b3fb5ce1c9
yiichiu <yic211@nyu.edu>
2015-04-06 13:52:07 -0400

    modify parse html

--- utils/parse_html.py
+++ utils/parse_html.py
@@ -1,1 +1,15 @@
 from lxml import html
+
+def parse_content(filename):
+    with open(filename, 'r') as infile:
+        data = infile.read().replace('\n', ' ')
+    
+    tree = html.fromstring(data)
+    for ele in list(list(list(list(tree)[1])[3])[9])[10].itertext():
+        if ele.strip() != '':
+            print ele.encode('utf-8')
+
+if __name__ == '__main__':
+    parse_content('../wiki/YouTube')
+
+
