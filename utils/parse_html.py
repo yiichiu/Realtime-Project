3bfedaa67354d397f4c6300688107d25df16188d
yiichiu <yic211@nyu.edu>
2015-04-09 18:12:04 -0400

    update parse html

--- utils/parse_html.py
+++ utils/parse_html.py
@@ -1,15 +1,25 @@
 from lxml import html
+from nltk.tokenize import RegexpTokenizer
 
 def parse_content(filename):
+    tokenizer = RegexpTokenizer(r'\w+')
+
     with open(filename, 'r') as infile:
         data = infile.read().replace('\n', ' ')
     
     tree = html.fromstring(data)
+    word_count = {}
     for ele in list(list(list(list(tree)[1])[3])[9])[10].itertext():
         if ele.strip() != '':
-            print ele.encode('utf-8')
+            for token in tokenizer.tokenize(ele.lower()):
+                if token not in word_count:
+                    word_count[token] = 0
+                word_count[token] += 1
+    for ele in word_count.keys():
+        if word_count[ele] > 10:
+            print ele, word_count[ele]
 
 if __name__ == '__main__':
-    parse_content('../wiki/YouTube')
+    parse_content('wiki/Google')
 
 
