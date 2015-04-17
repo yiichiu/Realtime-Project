b08e7cb55946dd3401ed337dbb63d4b4e4aa6d41
Po-Chih Lin <pcl322@172-16-227-216.DYNAPOOL.NYU.EDU>
2015-04-17 00:31:33 -0400

    DATA

--- utils/dataProcessing.py
+++ utils/dataProcessing.py
@@ -1,6 +1,9 @@
 import sys
 import os
 
+"""
+python dataProcessing wiki/ 20 dataser/out
+"""
 
 def main():
 
@@ -8,23 +11,30 @@ def main():
 
 	fileNames = os.walk(path).next()[2]
 
-	part = int(sys.argv[2])
-	numberOfSets = 10	
+	numberOfSet = int(sys.argv[2])
+
+	outputName = sys.argv[3]
+
+	fp = [open(outputName+".%d"%(i), "w") for i in range(numberOfSet)]
 
 	for i in range(len(fileNames)):
 
-		if i % numberOfSets == part: 
+		with open(path+fileNames[i]) as doc:
 
-			with open(path+fileNames[i]) as doc:
+			content = []
+			for line in doc:
+				content.append(line.replace("\n", ""))
 
-				content = []
-				for line in doc:
-					content.append(line.replace("\n", ""))
+			#print "%d\t"%(i)+" ".join(content)
 
-				print "%d\t"%(i)+" ".join(content)
-
+			fp[i % numberOfSet].write("%d\t"%(i)+" ".join(content)+"\n")
 
 
 if __name__ == "__main__":
 	main()
+
+
+
+
+
 
