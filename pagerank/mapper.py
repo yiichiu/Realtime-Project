2ec5106f8d061da918b24bbe745581d02ad8c149
yiichiu <yic211@nyu.edu>
2015-04-16 22:26:37 -0400

    add standalone url extraction

---
+++ pagerank/mapper.py
@@ -0,0 +1,26 @@
+from lxml import html
+import urlparse
+
+def parse_hyperlink(filename):
+    wiki_names = set([])
+    with open('../wiki_list', 'r') as infile:
+        data = infile.readlines()
+        for row in data:
+            wiki_names.add(row.strip())
+
+    with open(filename, 'r') as infile:
+        data = infile.read().replace('\n', ' ')
+
+    tree = html.fromstring(data)
+    linklist = tree.xpath('//a')
+    urls = []
+
+    for link in linklist:
+        if link.get('href'):
+            name = link.get('href')
+            if not (name.startswith('http') or name.startswith('.') or name.startswith('#')):
+                if name in wiki_names:
+                    print '%s\t%s' % ( name, filename.split('/')[-1] )
+    
+
+parse_hyperlink('../wiki/YouTube')
